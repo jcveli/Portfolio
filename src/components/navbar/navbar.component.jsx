db771272cb98c1e8ea981434d8238f757c964216
Jerald Velicaria <69361447+jcveli@users.noreply.github.com>
2022-09-24 21:02:04 -0700

    Revamped navigation bar to be responsive

--- src/components/navbar/navbar.component.jsx
+++ src/components/navbar/navbar.component.jsx
@@ -1,29 +1,83 @@
-import { Fragment } from 'react';
+import { CContainer, CNavbar, CCollapse, CNavbarToggler, CNavItem, CNavLink, CNavbarNav, CNavbarBrand } from '@coreui/react';
+import { Fragment,useState } from 'react';
 import {Outlet} from 'react-router-dom';
-
-//import { CNavbar } from '@coreui/react';
+import '@coreui/coreui/dist/css/coreui.min.css'
 import './navbar.styles.scss'
 
-const Navigation = () => { 
+const Navigation = () => {
+    //used to determine if the menu is open or not 
+    const [visible, setVisible] = useState(false);
+
+
+
     return (
-        <div>
-            <Fragment>
-                <div className='navigation-container'>
-                    <div className='nav-links-container'>                  
-                        <a className='nav-link' href='#about'>About</a>
-                        <a className='nav-link' href='#projects'>Projects</a>
-                        <a className='nav-link' href='#contact'>Contact</a>
-                        <a className='nav-link' href='https://www.linkedin.com/in/jvelicaria/' target="_blank" rel="noopener noreferrer">
-                            <i className="cib-linkedin icon icon-xxl"></i>
-                        </a>
-                        <a className='nav-link' href='https://github.com/jcveli' target="_blank" rel="noopener noreferrer">
-                            <i className="cib-github icon icon-xxl"></i>
-                        </a>
-                    </div>
-                </div>
-                <Outlet />
-            </Fragment>
-        </div>
+       
+        
+        <Fragment>
+            <CNavbar expand="lg" colorScheme='dark' className='navigation-container'>
+                <CContainer fluid>
+                    <CNavbarBrand />
+                    <CNavbarToggler
+                        aria-label="Toggle navigation"
+                        aria-expanded={visible}
+                        onClick={() => setVisible(!visible)}
+                        className="toggler-icon"
+                    />
+                    <CCollapse className='navbar-collapse' visible={visible}>
+                        <CNavbarNav className='nav-links-container'>
+                            <CNavItem>
+                            <CNavLink className='nav-link' href='#about'>
+                                About
+                            </CNavLink>
+                            </CNavItem>
+                            <CNavItem>
+                                <CNavLink className='nav-link' href='#projects'>
+                                    Projects
+                                </CNavLink>
+                            </CNavItem>
+                            <CNavItem>
+                                <CNavLink className='nav-link' href='#contact'>
+                                    Contact
+                                </CNavLink>
+                            </CNavItem>
+                            <CNavItem>
+                                <CNavLink className='nav-link' href='https://www.linkedin.com/in/jvelicaria/' target="_blank" rel="noopener noreferrer">
+                                <i className="cib-linkedin icon icon-xxl" />
+                                </CNavLink>
+                            </CNavItem>
+                            <CNavItem>
+                                <CNavLink className='nav-link' href='https://github.com/jcveli' target="_blank" rel="noopener noreferrer">
+                                    <i className="cib-github icon icon-xxl" />
+                                </CNavLink>
+                            </CNavItem>
+                        </CNavbarNav>
+                    </CCollapse>
+                </CContainer>
+            </CNavbar>
+            <Outlet />
+        </Fragment>
+
+        // <Fragment>
+        //     <div className='navigation-container'>
+        //         <div className='nav-links-container'>                  
+        //             <a className='nav-link' href='#about'>About</a>
+        //             <a className='nav-link' href='#projects'>Projects</a>
+        //             <a className='nav-link' href='#contact'>Contact</a>
+        //             <a className='nav-link' href='https://www.linkedin.com/in/jvelicaria/' target="_blank" rel="noopener noreferrer">
+        //                 <i className="cib-linkedin icon icon-xxl"></i>
+        //             </a>
+        //             <a className='nav-link' href='https://github.com/jcveli' target="_blank" rel="noopener noreferrer">
+        //                 <i className="cib-github icon icon-xxl"></i>
+        //             </a>
+
+        //             <div className='menu-icon'>
+        //                 <i></i>
+        //             </div>
+        //         </div>
+        //     </div>
+        //     <Outlet />
+        // </Fragment>
+        
     )
 }
 
