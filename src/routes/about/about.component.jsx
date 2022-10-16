e58c0041c876abdfe24f98db18f072a9ec7b2559
Jerald Velicaria <69361447+jcveli@users.noreply.github.com>
2022-10-15 22:15:59 -0700

    added skills section in about

--- src/routes/about/about.component.jsx
+++ src/routes/about/about.component.jsx
@@ -2,7 +2,8 @@ import './about.styles.scss';
 import Details from '../../components/about/about-details.component';
 import Profile from '../../components/profile/profile.component'
 
-
+import CIcon from '@coreui/icons-react';
+import { cibHtml5Shield, cibCss3Shiled, cibSass, cibJavascript, cibReact } from '@coreui/icons';
 
 const About = () => { 
     return (
@@ -10,8 +11,34 @@ const About = () => {
             <div className='about' id='about'> 
                 <div className='about-contents'>
                     <Profile />
-                    <Details />  
+                    <Details /> 
+                    
                 </div>
+                <div className='skills-grid'>
+                    <h1>Skills</h1>
+                    <div className='skills-list'>
+                        <div className='skill-icon'>
+                            <CIcon icon={cibHtml5Shield} size='xxl'/>
+                            <h2>HTML</h2>
+                        </div>
+                        <div className='skill-icon'>
+                            <CIcon icon={cibCss3Shiled } size='xxl'/>
+                            <h2>CSS</h2>
+                        </div>
+                        <div className='skill-icon'>
+                            <CIcon icon={cibSass} size='xxl'/>
+                            <h2>SASS</h2>
+                        </div>
+                        <div className='skill-icon'>
+                            <CIcon icon={cibJavascript} size='xxl'/>
+                            <h2>JavaScript</h2>
+                        </div>
+                        <div className='skill-icon'>
+                            <CIcon icon={cibReact} size='xxl'/>
+                            <h2>React</h2>
+                        </div>
+                    </div>
+                </div> 
             </div>
         
        
